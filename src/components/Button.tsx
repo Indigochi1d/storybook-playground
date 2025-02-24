dcce94820f100b4f0f2377cd69671506df691c9b
BeomSu Kim <33516980+Indigochi1d@users.noreply.github.com>
2025-02-25 05:01:48 +0900

    label 만들고 tailwind적용

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -3,17 +3,17 @@ import React from 'react';
 import './button.css';
 
 export interface ButtonProps {
-  /** Is this the principal call to action on the page? */
   primary?: boolean;
-  /** What background color to use */
   backgroundColor?: string;
-  /** How large should the button be? */
   size?: 'small' | 'medium' | 'large';
-  /** Button contents */
   label: string;
-  /** Optional click handler */
   onClick?: () => void;
-}
+} 
+/** 
+  지금 인터페이스에 적힌 주석들이 storybook에 반영되고 있는데 
+  스토리의 argTypes를 컴포넌트에 작성하는 것은 컴포넌틔 영역이 아니다.
+  따라서 meta/argTypes/YourProps/description 에 작성해야한다.
+**/
 
 /** Primary UI component for user interaction */
 export const Button = ({
--- src/stories/Button.stories.ts
+++ src/stories/Button.stories.ts
@@ -1,30 +1,26 @@
 import type { Meta, StoryObj } from '@storybook/react';
 import { fn } from '@storybook/test';
 
-import { Button } from './Button';
+import { Button } from '../components/Button';
 
-// More on how to set up stories at: https://storybook.js.org/docs/writing-stories#default-export
 const meta = {
-  title: 'Example/Button',
-  component: Button,
-  parameters: {
-    // Optional parameter to center the component in the Canvas. More info: https://storybook.js.org/docs/configure/story-layout
-    layout: 'centered',
+  title: 'Basic/Button',  //경로를 나타냄
+  component: Button,        //내가 이 컴포넌트의 스토리를 작성했다는 것을 알려줌
+  parameters: {             //스토리북의 동작을 커스터마이징하는 데 사용
+    layout: 'centered',     // 레이아웃 위치를 설정
   },
-  // This component will have an automatically generated Autodocs entry: https://storybook.js.org/docs/writing-docs/autodocs
   tags: ['autodocs'],
-  // More on argTypes: https://storybook.js.org/docs/api/argtypes
-  argTypes: {
-    backgroundColor: { control: 'color' },
+  argTypes: {               // 스토리북에서 컴포넌트의 props(인자)들을 어떻게 조작할지 정의하는 설정
+    primary: { control: 'boolean', description: '버튼의 타입을 지정해주세요.' },
+    backgroundColor: { 
+      control: 'color', description:'버튼의 배경색을 지정해주세요.'},  
   },
-  // Use `fn` to spy on the onClick arg, which will appear in the actions panel once invoked: https://storybook.js.org/docs/essentials/actions#action-args
-  args: { onClick: fn() },
+  args: { onClick: fn() },  //스토리에서 사용할 기본 props 값을 미리 정의
 } satisfies Meta<typeof Button>;
 
 export default meta;
 type Story = StoryObj<typeof meta>;
 
-// More on writing stories with args: https://storybook.js.org/docs/writing-stories/args
 export const Primary: Story = {
   args: {
     primary: true,
