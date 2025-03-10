6bd776b30ae92fe7ccc4805bbe4e6b3c70550fce
BeomSu Kim <33516980+Indigochi1d@users.noreply.github.com>
2025-03-11 08:45:15 +0900

    CategoryButton darkmode

--- src/components/CategoryButton.tsx
+++ src/components/CategoryButton.tsx
@@ -6,15 +6,24 @@ interface ICategoryButtonProps {
     text:string;
     onClick: React.MouseEventHandler<HTMLButtonElement>;
     isDisabled?:boolean;
+    isDarkMode?:boolean;
 }
 
 const disabled = "disabled:bg-mono100 disabled:text-mono300";
 
-const CategoryButton = ({iconPath,iconAlt,text,onClick,isDisabled}:ICategoryButtonProps) => {
+const CategoryButton = ({iconPath,iconAlt,text,onClick,isDisabled,isDarkMode}:ICategoryButtonProps) => {
     return (
-        <button onClick={onClick} disabled={isDisabled} className={`${disabled} flex-col justify-items-center rounded-[10px] w-20 h-20`}>
+        <button
+         onClick={onClick} 
+         disabled={isDisabled} 
+         className={`${disabled} 
+         flex-col 
+         justify-items-center 
+         rounded-[10px] 
+         w-20 h-20
+         ${isDarkMode ? "bg-primary" : "bg-white"}`}>
             <img src={iconPath} alt={iconAlt} className={`${isDisabled ? "opacity-60" : ""} `}/>
-            <p className={`text-primary font-extrabold ${isDisabled ? "opacity-60" : ""}`}>{text}</p>
+            <p className={`text-primary font-extrabold ${isDisabled ? "opacity-60" : ""} ${isDarkMode? "text-white" : "text-primary"}`}>{text}</p>
         </button>
     );
 };
